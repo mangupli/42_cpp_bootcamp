61520fe6d437eb781f5580fed5657be64de4560a
mangupli <gaponova.liz.m@gmail.com>
2021-06-08 12:27:12 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/my_piscine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_piscine.dir/main.cpp.o"
+  "my_piscine"
+  "my_piscine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_piscine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
