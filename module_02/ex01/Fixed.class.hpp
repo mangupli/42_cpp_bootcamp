7c21d3070ecb87cd48dc39018d2f48ba7e0a77b6
mangupli <gaponova.liz.m@gmail.com>
2021-07-29 00:12:38 +0300

    working with fix value

---
+++ module_02/ex01/Fixed.class.hpp
@@ -0,0 +1,29 @@
+#ifndef FIXED_CLASS_H
+# define FIXED_CLASS_H
+
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+
+private:
+
+	static int const	_fractionalBits = 8;
+	int 				_value;
+
+public:
+
+	Fixed( void );
+	Fixed ( int const value );
+	Fixed ( float const value );
+	Fixed(Fixed const &src);
+	~Fixed( void );
+
+	int			getRawBits( void ) const;
+	void		setRawBits( int const rawBits );
+
+	Fixed &		operator=( Fixed const & equalTo );
+
+};
+
+#endif
