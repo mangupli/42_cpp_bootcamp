d308cc05ebf7404a44005f095fb70d6d2e7e42f7
mangupli <gaponova.liz.m@gmail.com>
2021-08-10 13:20:06 +0300

    Flagtrap class created and finished ex02

---
+++ module_03/ex02/ClapTrap.class.hpp
@@ -0,0 +1,45 @@
+#ifndef CLAPTRAP_CLASS_H
+# define CLAPTRAP_CLASS_H
+
+# include <iostream>
+
+class ClapTrap {
+
+protected:
+
+	std::string			_name;
+	unsigned int		_hitPoints;
+	unsigned int		_energyPoints;
+	unsigned int		_attackDamage;
+
+public:
+
+	ClapTrap( void );
+	ClapTrap( std::string const name );
+	ClapTrap( ClapTrap const & other );
+	~ClapTrap( void );
+
+	ClapTrap & operator=( ClapTrap const & equalTo );
+
+	void			attack(std::string const & target) const;
+	void			takeDamage(unsigned int amount);
+	void			beRepaired(unsigned int amount);
+
+	void			printAttributes( void );
+
+	std::string		getName( void ) const;
+	unsigned int	getHitPoints( void ) const;
+	unsigned int	getEnergyPoints( void ) const;
+	unsigned int	getAttackDamage( void ) const;
+
+	void			setName( std::string const name );
+	void			setHitPoints( unsigned int hitPoints );
+	void			setEnergyPoints( unsigned int energyPoints );
+	void			setAttackDamage( unsigned int attackDamage );
+	void 			setIntAttributes(unsigned int hitPoints,
+						  					unsigned int energyPoints,
+						  						unsigned int attackDamage);
+
+};
+
+#endif
